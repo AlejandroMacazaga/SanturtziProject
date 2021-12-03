f5a1a845cfddc7849700184a29f18ad868197545
Val <nereadelvalle77@gmail.com>
2021-12-03 14:19:36 +0100

    Cancion1
    Pues ns

---
+++ app/src/main/java/com/example/santurtzi/FakeMapa.java
@@ -0,0 +1,23 @@
+package com.example.santurtzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class fakeMapa extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_mapa);
+    }
+
+    public void irG1(View v)
+    {
+        Intent intent=new Intent(,)
+    }
+
+}
---
+++ app/src/main/java/com/example/santurtzi/G1_1.java
@@ -0,0 +1,14 @@
+package com.example.santurtzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class G1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g11);
+    }
+}
