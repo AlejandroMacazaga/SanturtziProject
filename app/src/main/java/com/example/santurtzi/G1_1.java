27374e22f3cfe06e4a8e7d24139dbdc0f749ab7d
Val <nereadelvalle77@gmail.com>
2021-12-13 14:26:41 +0100

    Actividad1
    Primera parte casi terminada

--- app/src/main/java/com/example/santurtzi/G1_1.java
+++ app/src/main/java/com/example/santurtzi/G1_1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class G1_1 extends AppCompatActivity
 {
@@ -19,6 +20,11 @@ public class G1_1 extends AppCompatActivity
         clPrincipal=findViewById(R.id.clPrincipal);
         clPrincipal.setBackgroundResource(R.mipmap.g1fondo1_foreground);
 
+    }
+
+    public void cambiarFragment()
+    {
+        Log.i("pruebax","Funciona");
     }
 
 }
