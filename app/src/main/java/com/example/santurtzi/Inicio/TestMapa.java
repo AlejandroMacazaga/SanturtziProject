235c4592bfd1442af2c1240492fb2b4a98ab0526
Aitor <aitor51@outlook.com>
2022-01-24 09:04:23 +0100

    mapa
    asies mapa

--- app/src/main/java/com/example/santurtzi/Inicio/TestMapa.java
+++ app/src/main/java/com/example/santurtzi/Inicio/TestMapa.java
@@ -3,7 +3,8 @@ package com.example.santurtzi.Inicio;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.santurtzi.R;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.maps.MapView;
 
 
 import android.os.Bundle;
@@ -15,26 +16,22 @@ public class TestMapa extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this,getString(R.string.token_publico));
+        Mapbox.getInstance(this,getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_test_mapa);
         mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback()){
+
+        }
+
     }
     @Override
     protected void onStart(){
         super.onStart();
         mapView.onStart();
     }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        mapView.onResume();
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        mapView.onPause();
-    }
+
+
     @Override
     protected void onStop(){
         super.onStop();
@@ -43,7 +40,7 @@ public class TestMapa extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        //mapView.onSaveInstanceState(outState);
     }
     @Override
     public void onLowMemory(){
